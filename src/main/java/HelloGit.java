7d17b37ffda9f28b6119d6fd7c8ef3004db25e92
zmz <1484387288@qq.com>
2022-04-19 13:55:38 +0800

    hot-fix first commit

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
         System.out.println("Hello Git!!!");
         System.out.println("Hello Git1!!!");
         System.out.println("Hello Git2!!!");
+        System.out.println("hot-fix test");
     }
 }
